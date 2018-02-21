03adfef4fbc1ef339fc2e61ee7605d5ed5dc3463
Axelle Feider <axellefeider@Axelles-MacBook-Pro-2.local>
2018-02-21 14:15:54 +0100

    DocsController

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,22 @@
+class DocsController < ApplicationController
+  def show
+    # ...
+  end
+  def edit
+    # ...
+  end
+  def update
+    # ...
+  end
+  def destroy
+  # ...
+  end
+
+  private
+
+  def find_doc
+  end
+
+  def doc_params
+  end
+end 
